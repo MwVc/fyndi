f8d9328bd5242b0c9520d3b3bd4781b57a17b8b3
Victor Mwadime <victormwadime@gmail.com>
2025-08-06 09:34:41 +0300

    Import controllers for services into service router

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { createService } = require("../controllers/serviceController");
+const {
+  createService,
+  getAllServices,
+  getServiceById,
+  updateService,
+} = require("../controllers/serviceController");
 const verifyToken = require("../middleware/verifyToken");
 
+router.get("/", getAllServices);
 router.post("/create", verifyToken, createService);
+router.get("/:id", getServiceById);
+router.put("/:id", verifyToken, updateService);
 
 module.exports = router;
