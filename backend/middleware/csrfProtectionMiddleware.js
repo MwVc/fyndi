6a9c1f7c173df0bd71dd30b904aae54ed621421a
Victor Mwadime <victormwadime@gmail.com>
2025-12-24 10:59:24 +0300

    feat: add csrf protection middleware to validate x-csrf-token header agains csrf_token cookie

---
+++ backend/middleware/csrfProtectionMiddleware.js
@@ -0,0 +1,12 @@
+const csrfProtection = (req, res, next) => {
+  const csrfCookie = req.cookies.csrf_token;
+  const csrfHeader = req.headers["x-csrf-token"];
+
+  if (!csrfCookie || !csrfHeader || csrfCookie !== csrfHeader) {
+    return res.status(403).json({ error: "CSRF detected" });
+  }
+
+  next();
+};
+
+module.exports = { csrfProtection };
