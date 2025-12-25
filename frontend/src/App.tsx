845bf7a45104bd315de6a09489a7805be8437fe6
Victor Mwadime <victormwadime@gmail.com>
2025-12-25 10:09:16 +0300

    refactor: replace login mockup with main dashboard architecture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,17 @@
-import { Button } from "@/components/ui/button";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldSet,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+import BottomNav from "./components/layout/BottomNav";
+import Home from "./pages/Home";
+import TopBar from "./components/layout/TopBar";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <div className="w-full max-w-md">
-        <FieldSet>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="username">Username</FieldLabel>
-              <Input id="username" type="text" placeholder="Max Leiter" />
-              <FieldDescription>
-                Choose a unique username for your account.
-              </FieldDescription>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <FieldDescription>
-                Must be at least 8 characters long.
-              </FieldDescription>
-              <Input id="password" type="password" placeholder="••••••••" />
-            </Field>
-          </FieldGroup>
-        </FieldSet>
+    <div className="min-h-screen bg-gray-50">
+      <TopBar />
+      {/* centered app container */}
+      <div className="mx-auto max-w-6xl px-4 pb-20">
+        <Home />
       </div>
-      <div className="w-full max-w-md m-3 flex items-center justify-center">
-        <Button>Login</Button>
-      </div>
+
+      <BottomNav />
     </div>
   );
 }
