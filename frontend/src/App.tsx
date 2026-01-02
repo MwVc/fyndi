1fd6140b18a381fd74ef35778f22f32e4d4fd967
Victor Mwadime <victormwadime@gmail.com>
2026-01-02 10:18:47 +0300

    feat: create login functionality using fetch request to backend api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
 import { useEffect } from "react";
 import Layout from "./pages/Layout";
+import Login from "./pages/Login";
 
 const App = () => {
+  const onLogin = async (email: string, password: string) => {
+    try {
+      const response = await fetch("https://localhost:5050/", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(
     () => document.documentElement.setAttribute("data-theme", "light"),
     []
   );
 
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <Login onLogin={onLogin} />
+    </>
+  );
 };
 
 export default App;
