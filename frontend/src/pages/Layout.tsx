c5763af8ac2a2d3e77d459159b83031e92cd92ab
Victor Mwadime <victormwadime@gmail.com>
2026-01-02 10:22:53 +0300

    remove debug border from Layout container

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen border border-red-500">
+    <div className="min-h-screen">
       <Navbar />
     </div>
   );
