6e9e098f5d4adc8a765673a2c4d2a2a365c703b7
Victor Mwadime <victormwadime@gmail.com>
2026-01-15 15:26:32 +0300

    feat:add auth router and global error middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,17 +2,26 @@ import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRouter.js";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 // middleware
-app.use(cookieParser());
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 // routes
 app.get("/", (req: Request, res: Response) => {
   res.send("API is runnning");
 });
+
+// authRouter middleware
+app.use("/auth", authRouter);
+
+app.use(errorMiddleware);
+
+console.log("Hello from app.ts");
 
 export default app;
