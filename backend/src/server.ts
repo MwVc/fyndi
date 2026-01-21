55703679060a1fe3ea58f1a99f5b07f54ce0976b
Victor Mwadime <victormwadime@gmail.com>
2026-01-21 12:05:16 +0300

    feat(server): use HTTPS in development and HTTP in production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,24 @@
 import app from "./app.js";
 import { config } from "dotenv";
+import { readFileSync } from "fs";
+import https from "https";
+import path from "path";
 
 config(); // load enviroment variables
 
 const PORT = process.env.PORT;
+const isProd = process.env.NODE_ENV;
 
-app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
+if (isProd === "production") {
+  app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
+} else {
+  // create a https server when in dev mode
+  const options = {
+    key: readFileSync("../certs/localhost+2-key.pem", "utf8"),
+    cert: readFileSync("../certs/localhost+2.pem", "utf8"),
+  };
+
+  const httpsServer = https
+    .createServer(options, app)
+    .listen(PORT, () => console.log(`HTTPS server is listening on ${PORT}`));
+}
