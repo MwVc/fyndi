bb832282e7b5ef432e5319230e2a7a5e3dee6c77
Victor Mwadime <victormwadime@gmail.com>
2026-01-21 17:55:08 +0300

    feat(auth): update login request and add logout API call

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,11 +1,27 @@
 export const onLogin = async (email: string, password: string) => {
   try {
-    const response = await fetch("https://localhost:5050/", {
-      method: "GET",
+    const response = await fetch("https://localhost:5050/auth/login", {
+      method: "POST",
       headers: { "Content-Type": "application/json" },
-      credentials: "include",
+      credentials: "include", // include cookies
       body: JSON.stringify({ email, password }),
     });
+
+    console.log(response);
+
+    console.log(document.cookie);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const onLogout = async () => {
+  try {
+    const response = await fetch("https://localhost:5050/auth/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    return response;
 
     console.log(response);
   } catch (error) {
