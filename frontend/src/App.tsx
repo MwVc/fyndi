d2a4d94bbd3700511604ca80440001ba671dce07
Victor Mwadime <victormwadime@gmail.com>
2026-01-23 22:58:46 +0300

    refactor: remove unused onLogin prop from Login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
 import { useEffect } from "react";
 import Layout from "./pages/Layout";
 import Login from "./pages/Login";
-import { onLogin } from "./api/auth";
 
 const App = () => {
   useEffect(
     () => document.documentElement.setAttribute("data-theme", "light"),
-    []
+    [],
   );
 
   return (
     <>
       <Layout />
-      <Login onLogin={onLogin} />
+      <Login />
     </>
   );
 };
