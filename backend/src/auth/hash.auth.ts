101e6ef26e0a42576f4e2446fce594143867495a
Victor Mwadime <victormwadime@gmail.com>
2026-02-09 11:47:31 +0300

    feat(auth): add password comparison and error handling for hashing

--- backend/src/auth/hash.auth.ts
+++ backend/src/auth/hash.auth.ts
@@ -1,8 +1,40 @@
 import bcrypt from "bcrypt";
+import ApiError from "../errors/api.errors.js";
+import { SystemErrorCodes } from "../errors/code.errors.js";
 
 export const hashPassword = async (password: string): Promise<string> => {
   // hash password
   const saltRounds = 10;
-  const hash = await bcrypt.hash(password, saltRounds);
-  return hash;
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    return hash;
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(
+      500,
+      SystemErrorCodes.PASSWORD_HASH_FAILED,
+      "Internal Server Error",
+      false,
+      error,
+    );
+  }
+};
+
+export const compare = async (
+  plainPassword: string,
+  hashedPassword: string,
+): Promise<boolean> => {
+  try {
+    const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    return isMatch;
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(
+      500,
+      SystemErrorCodes.PASSWORD_COMPARE_FAILED,
+      "Internal Server Error",
+      false,
+      error,
+    );
+  }
 };
