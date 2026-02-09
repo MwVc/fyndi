7935d85ec41f623c2d45c46a09cd755065cebb1d
Victor Mwadime <victormwadime@gmail.com>
2026-02-09 11:45:57 +0300

    feat(db): replace full_name with first_name and last_name in users table

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -5,7 +5,8 @@ DROP TABLE IF EXISTS refresh_tokens;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
     role VARCHAR(20) DEFAULT 'user',
