e596e207b41c35ab7a7e8d583fd6512aea48984a
Victor Mwadime <victormwadime@gmail.com>
2026-02-11 14:45:45 +0300

    feat(user-service): add login function and integrate authentication utilities

--- backend/src/services/users.services.ts
+++ backend/src/services/users.services.ts
@@ -1,9 +1,9 @@
-import { models } from "../models/users.models.js";
-import { hashPassword } from "../auth/hash.auth.js";
+import { userModels } from "../models/users.models.js";
+import { authentication } from "../auth/hash.auth.js";
 import ApiError from "../errors/api.errors.js";
-import { SystemErrorCodes, UserErrorCodes } from "../errors/code.errors.js";
-import { error } from "node:console";
-import { errorResponse } from "../utilities/response.js";
+import { UserErrorCodes } from "../errors/code.errors.js";
+import { ValidationErrorCodes } from "../errors/code.errors.js";
+import { signAccessToken, signRefreshToken } from "../auth/token.auth.js";
 
 export interface RegisterUserData {
   firstName: string;
@@ -19,12 +19,12 @@ const create = async ({
   email,
   password,
 }: RegisterUserData) => {
-  const hashedPassword = await hashPassword(password);
+  const hashedPassword = await authentication.hashPassword(password);
   console.log(hashedPassword);
 
   // utilise try/catch to catch user exists error
   try {
-    const user = await models.insert({
+    const user = await userModels.insert({
       firstName,
       lastName,
       email,
@@ -46,6 +46,44 @@ const create = async ({
   }
 };
 
-export const users = {
+const login = async (email: string, password: string) => {
+  // get user from database
+  const user = await userModels.getByEmail(email);
+  console.log(user);
+  if (!user) {
+    throw new ApiError(
+      400,
+      ValidationErrorCodes.INVALID_EMAIL_FORMAT,
+      "Invalid Email",
+      true,
+    );
+  }
+
+  // validate password
+  const isMatch = await authentication.comparePassword(password, user.password);
+
+  if (!isMatch) {
+    throw new ApiError(
+      400,
+      ValidationErrorCodes.INVALID_PASSWORD_FORMAT,
+      "Invalid password",
+      true,
+    );
+  }
+
+  // generate JWT
+  const payload = { id: user.id, role: user.role };
+
+  // create tokens
+  const accessToken = signAccessToken(payload);
+  const refreshToken = signRefreshToken(payload);
+  const csrfToken = crypto.randomUUID();
+  console.log(csrfToken);
+
+  return { user, accessToken, refreshToken, csrfToken };
+};
+
+export const userServices = {
   create,
+  login,
 };
