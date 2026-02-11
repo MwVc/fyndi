461f80cedea6245e4eb4b781353ea5ce73d5c0a0
Victor Mwadime <victormwadime@gmail.com>
2026-02-11 09:35:00 +0300

    refactor(user.service): move hashing and db logic to service layer with structured error handling

--- backend/src/services/users.services.ts
+++ backend/src/services/users.services.ts
@@ -1,23 +1,51 @@
-import type { User } from "../models/user.js";
-import pool from "../db/index.js";
+import { models } from "../models/users.models.js";
+import { hashPassword } from "../auth/hash.auth.js";
+import ApiError from "../errors/api.errors.js";
+import { SystemErrorCodes, UserErrorCodes } from "../errors/code.errors.js";
+import { error } from "node:console";
+import { errorResponse } from "../utilities/response.js";
 
-export const createuser = async ({
-  firstName,
-  lastName,
-  email,
-  hashedPassword,
-}: {
+export interface RegisterUserData {
   firstName: string;
   lastName: string;
   email: string;
-  hashedPassword: string;
-}) => {
+  password?: string;
+  hashedPassword?: string;
+}
+
+const create = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}: RegisterUserData) => {
+  const hashedPassword = await hashPassword(password);
+  console.log(hashedPassword);
+
+  // utilise try/catch to catch user exists error
   try {
-    const dbResponse = await pool.query(
-      "INSERT INTO users (full_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
-      [firstName, lastName, email, hashedPassword],
-    );
-  } catch (error) {
-    console.log(error);
+    const user = await models.insert({
+      firstName,
+      lastName,
+      email,
+      hashedPassword,
+    });
+
+    return user;
+  } catch (error: any) {
+    if (error.code === "23505") {
+      throw new ApiError(
+        400,
+        UserErrorCodes.EMAIL_EXISTS,
+        "email exists",
+        true,
+      );
+    }
+
+    throw error;
   }
+};
+
+export const users = {
+  create,
 };
