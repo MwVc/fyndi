6de37e85007a8d5a164522e0c017072bd85ea2ae
Victor Mwadime <victormwadime@gmail.com>
2026-02-16 11:33:20 +0300

    refactor(users.model): enforce required hashedPassword in insert model

--- backend/src/models/users.models.ts
+++ backend/src/models/users.models.ts
@@ -6,7 +6,12 @@ const insert = async ({
   lastName,
   email,
   hashedPassword,
-}: RegisterUserData) => {
+}: {
+  firstName: string;
+  lastName: string;
+  email: string;
+  hashedPassword: string;
+}) => {
   const { rows } = await pool.query(
     "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
     [firstName, lastName, email, hashedPassword],
