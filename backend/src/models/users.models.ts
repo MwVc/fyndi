bc14021caa27f0a369e7af1e22104637dc98215f
Victor Mwadime <victormwadime@gmail.com>
2026-02-20 12:42:32 +0300

    refactor(user-models): rename hashedPassword to password for consistency

--- backend/src/models/users.models.ts
+++ backend/src/models/users.models.ts
@@ -1,26 +1,25 @@
-import type { RegisterUserData } from "../services/users.services.js";
+// import type { RegisterUserData } from "../services/users.services.js";
 import pool from "../db/pgPool.db.js";
 
 const insert = async ({
   firstName,
   lastName,
   email,
-  hashedPassword,
+  password,
 }: {
   firstName: string;
   lastName: string;
   email: string;
-  hashedPassword: string;
+  password: string;
 }) => {
   const { rows } = await pool.query(
     "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
-    [firstName, lastName, email, hashedPassword],
+    [firstName, lastName, email, password],
   );
   return rows[0];
 };
 
 const getByEmail = async (email: string) => {
-  // check if user exists
   const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [
     email,
   ]);
