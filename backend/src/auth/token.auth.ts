dfa30dfceb0afde28b9ecb51b3f6b3cfccc7c44e
Victor Mwadime <victormwadime@gmail.com>
2026-02-23 14:07:34 +0300

    refactor(auth): include maxAge and expiration metadata in access and refresh token generators

--- backend/src/auth/token.auth.ts
+++ backend/src/auth/token.auth.ts
@@ -1,3 +1,5 @@
+import { create } from "domain";
+import { access } from "fs";
 import jwt from "jsonwebtoken";
 
 // types
@@ -10,14 +12,32 @@ export const signAccessToken = (payload: JwtPayload) => {
   if (!process.env.JWT_ACCESS_SECRET) {
     throw new Error("JWT_SECRET mising");
   }
-  return jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-    expiresIn: "1m",
+  const token = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: "15m",
   });
+
+  return {
+    token: token,
+    maxAge: 15 * 60 * 1000, // 15min in milliseconds
+  };
 };
 
 export const signRefreshToken = (payload: JwtPayload) => {
   if (!process.env.JWT_REFRESH_SECRET) {
     throw new Error("JWT_SECRET mising");
   }
-  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+
+  const token = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
+  const createdAt = Date.now(); // milliseconds
+  const maxAge = 7 * 24 * 60 * 60 * 1000;
+  const expiresAt = createdAt + maxAge;
+
+  return {
+    token: token,
+    createdAt: createdAt,
+    maxAge: maxAge, // 7days in milliseconds
+    expiresAt: expiresAt,
+  };
 };
