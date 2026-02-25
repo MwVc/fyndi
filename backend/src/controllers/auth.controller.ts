ebbface5d987d169b8974073a0a3f11c6361d296
Victor Mwadime <victormwadime@gmail.com>
2026-02-25 14:08:43 +0300

    refactor(auth-controller): remove unnecessary try/catch in controllers and rely on express async error handling for cleaner code

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -16,15 +16,12 @@ export const registerUser = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    const data = req.body as RegisterUserData;
-    const user = await userServices.create(data);
+  const data = req.body as RegisterUserData;
 
-    // don't send user data to frontend upon creation
-    successResponse(res, 201, null, "user created successfully");
-  } catch (error) {
-    next(error);
-  }
+  await userServices.create(data); // awaiting service and db layer
+
+  // don't send user data to frontend upon creation
+  successResponse(res, 201, null, "user created successfully");
 };
 
 export const loginUser = async (
@@ -32,36 +29,32 @@ export const loginUser = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    // destructure email and password from req.body
-    const { email, password }: { email: string; password: string } = req.body;
-    // console.log("login func/controller:", email, password);
+  // destructure email and password from req.body
+  const { email, password }: { email: string; password: string } = req.body;
+  // console.log("login func/controller:", email, password);
 
-    const userData = await userServices.login(email, password);
-    // console.log(userData);
-    res
-      .cookie("access_token", userData.accessToken.token, {
-        httpOnly: true, // client side js can't read
-        secure: true, // htpps
-        sameSite: "none",
-        maxAge: userData.accessToken.maxAge, // time in milliseconds
-      })
-      .cookie("refresh_token", userData.refreshToken.token, {
-        httpOnly: true, // client side js can't read
-        secure: true, // https
-        sameSite: "none",
-        maxAge: userData.refreshToken.maxAge, // time in milliseconds
-      })
-      .cookie("csrf_token", userData.csrfToken, {
-        httpOnly: false, // exposing to client side js
-        sameSite: "none", // cross-site request
-        secure: true,
-      });
+  const userData = await userServices.login(email, password);
+  // console.log(userData);
+  res
+    .cookie("access_token", userData.accessToken.token, {
+      httpOnly: true, // client side js can't read protect agains XSS attacks
+      secure: true, // htpps
+      sameSite: "none",
+      maxAge: userData.accessToken.maxAge, // time in milliseconds
+    })
+    .cookie("refresh_token", userData.refreshToken.token, {
+      httpOnly: true, // client side js can't read
+      secure: true, // https
+      sameSite: "none",
+      maxAge: userData.refreshToken.maxAge, // time in milliseconds
+    })
+    .cookie("csrf_token", userData.csrfToken, {
+      httpOnly: false, // exposing to client side js
+      sameSite: "none", // cross-site request
+      secure: true,
+    });
 
-    successResponse(res, 200, null, "Logged in");
-  } catch (error) {
-    next(error);
-  }
+  successResponse(res, 200, null, "Logged in");
 };
 
 export const logoutUser = (req: Request, res: Response) => {
@@ -71,13 +64,11 @@ export const logoutUser = (req: Request, res: Response) => {
       httpOnly: true, // client side js can't read
       secure: true, // htpps
       sameSite: "none",
-      maxAge: 15 * 60 * 1000,
     })
     .cookie("refresh_token", "", {
       httpOnly: true, // client side js can't read
       secure: true, // https
       sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .cookie("csrf_token", "", {
       httpOnly: false, // exposing to client side js
