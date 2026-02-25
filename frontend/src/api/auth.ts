89722944f133f9d89e7f34a00720e20c4e794817
Victor Mwadime <victormwadime@gmail.com>
2026-02-25 23:14:33 +0300

    feat(user auth api): add registerUser function

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,24 +1,52 @@
 import apiClient from "./apiClient";
 import type { ApiResult } from "./types";
 
-export const onLogin = async (
-  email: string,
-  password: string,
-): Promise<ApiResult<null>> => {
-  const result: ApiResult<null> = await apiClient.post("/auth/login", {
+interface userData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export const registerUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}: userData) => {
+  const response: ApiResult<null> = await apiClient.post("/auth/register", {
+    firstName: firstName,
+    lastName: lastName,
     email: email,
     password: password,
   });
 
-  if (!result.success) {
-    console.log(result);
+  console.log(response);
+  return response;
+};
+
+export const onLogin = async (
+  email: string,
+  password: string,
+): Promise<ApiResult<null>> => {
+  const response: ApiResult<null> = await apiClient.post("/auth/login", {
+    email: email,
+    password: password,
+  });
+
+  if (!response.success) {
+    console.log(response);
   }
 
-  return result;
+  return response;
 };
 
 export const onLogout = async (): Promise<ApiResult<null>> => {
   const result: ApiResult<null> = await apiClient.post("auth/logout");
 
   return result;
+};
+
+export const hitServer = async () => {
+  return await apiClient.get("/");
 };
