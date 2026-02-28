0d7fb04a3457c03ba497570c234080279970faf7
Victor Mwadime <victormwadime@gmail.com>
2026-02-28 15:13:06 +0300

    chore(ratelimiter): change comments for better description

--- backend/src/middleware/rate_limit.middleware.ts
+++ backend/src/middleware/rate_limit.middleware.ts
@@ -3,8 +3,8 @@ import ApiError from "../errors/api.errors.js";
 import { PermissionErrorCodes, UserErrorCodes } from "../errors/code.errors.js";
 
 export const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // this is 5 minutes
-  limit: 10, // each IP can make 10 requests as per the time window ie 5 min
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 10, // each IP can make 10 requests as per the time window which is 5 min
   standardHeaders: true, // add 'RateLimit-*' headers to the response being sent
   legacyHeaders: false, // don't send old X-RateLimit-* headers
 
