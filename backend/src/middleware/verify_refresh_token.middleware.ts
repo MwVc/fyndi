28a852b36b51d5376d294ff6f99d50a40beeab24
Victor Mwadime <victormwadime@gmail.com>
2026-03-02 13:07:15 +0300

    feat(verify refresh token middleware): create a middleware to verify refresh token

---
+++ backend/src/middleware/verify_refresh_token.middleware.ts
@@ -0,0 +1,35 @@
+import type { NextFunction, Request, Response } from "express";
+import { errorResponse } from "../utilities/response.js";
+import { AuthErrorCodes } from "../errors/code.errors.js";
+import ApiError from "../errors/api.errors.js";
+import jwt from "jsonwebtoken";
+
+export const verifyRefreshToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log("Log from verify refresh token middleware", req.cookies);
+  const { refresh_token } = req.cookies;
+
+  if (!refresh_token) {
+    return errorResponse(res, 401, AuthErrorCodes.UNAUTHORIZED, "Unauthorized");
+  }
+
+  if (!process.env.JWT_REFRESH_SECRET) {
+    throw new ApiError(
+      NaN,
+      AuthErrorCodes.JWT_SECRET_UNDEFINED,
+      "Check .env file",
+      false,
+    );
+  }
+
+  try {
+    const user = jwt.verify(refresh_token, process.env.JWT_REFRESH_SECRET);
+    next();
+  } catch (error) {
+    console.log(error);
+    return errorResponse(res, 401, AuthErrorCodes.UNAUTHORIZED, "Unauthorized");
+  }
+};
