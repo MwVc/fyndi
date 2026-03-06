f4f7591454f745a6c5ef36c44c6b21ad872da96f
Victor Mwadime <victormwadime@gmail.com>
2026-03-06 11:37:41 +0300

    refactor(auth route): rename verifyRefreshToken middleware to refreshTokenMiddleware in auth routes

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "./auth.controller.js";
 import { logoutUser } from "./auth.controller.js";
-import { verifyRefreshToken } from "./middlewares/verify_refresh_token.middleware.js";
+import { refreshTokenMiddleware } from "./middlewares/verify_refresh_token.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
-authRouter.post("/refresh", verifyRefreshToken, (req, res) => {
+authRouter.post("/refresh", refreshTokenMiddleware, (req, res) => {
   console.log("refresh endpont being hit");
   res.send("The check was succesfull and the refresh token works");
 });
