664d176a2b2d155c6ee05899137db77d88fa2485
Victor Mwadime <victormwadime@gmail.com>
2026-03-18 11:54:31 +0300

    refactor(pgPool): improve log message

--- backend/src/infrastructure/db/pgPool.db.ts
+++ backend/src/infrastructure/db/pgPool.db.ts
@@ -10,7 +10,7 @@ const pool = new Pool({
 });
 
 pool.on("connect", () => {
-  console.log("Connected to Posgresql");
+  console.log("pgPool.ts: Connected to Posgresql\n");
 });
 
 export default pool;
