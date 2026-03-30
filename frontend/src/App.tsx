2adabcc4bfda5f6ab987fc66ba5ef811c84a4e63
Victor Mwadime <victormwadime@gmail.com>
2026-03-30 17:11:43 +0300

    feat(app.tsx): add auth session check on app load using refresh endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import { useEffect } from "react";
 import Layout from "./pages/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { refreshLogin } from "./api/auth";
+import { useContext } from "react";
+import { UserContext } from "./context/UserProvider";
 
 const App = () => {
+  const { setIsLoggedin } = useContext(UserContext);
+
+  const checkLogin = async () => {
+    const response = await refreshLogin();
+
+    if (!response.success) {
+      return;
+    }
+
+    setIsLoggedin(true);
+  };
+
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", "light");
+
+    // refresh login on app start
+    checkLogin();
   }, []);
 
   return (
