836f7d188e6c6a790b1b6f7e474ab5c12357b8fa
Victor Mwadime <victormwadime@gmail.com>
2026-04-04 23:33:17 +0300

    feat: add Main component to render a grid of JobCard components

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,18 @@
+import JobCard from "./JobCard";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const Main = () => {
+  return (
+    <main className="mx-auto w-full md:w-7/12">
+      {/* list of jobs */}
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 ">
+        {numbers.map((number) => (
+          <JobCard key={number} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Main;
