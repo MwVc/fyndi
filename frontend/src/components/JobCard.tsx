5d47b99a906239c4a6feecce7aa39464886ed6d6
Victor Mwadime <victormwadime@gmail.com>
2026-04-04 23:30:50 +0300

    feat: add JobCard component with image, title, description and action button

---
+++ frontend/src/components/JobCard.tsx
@@ -0,0 +1,24 @@
+const JobCard = () => {
+  return (
+    <div className="card bg-base-100 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Job Title</h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">View</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
