b9dc247aa93261ddd2b791a17420321c4c33655f
Victor Mwadime <victormwadime@gmail.com>
2026-04-13 10:20:32 +0300

    feat(ImageCarousel): add reusable ImageCarousel component with daisyUI carousel

---
+++ frontend/src/components/ImageCarousel.tsx
@@ -0,0 +1,32 @@
+const ImageCarousel = ({
+  images,
+}: {
+  images: { url: string; id: number }[];
+}) => {
+  const total = images.length;
+  return (
+    <div className="carousel w-full">
+      {images.map((image, index) => {
+        const prevIndex = (index - 1 + total) % total;
+        const nextIndex = (index + 1) % total;
+
+        return (
+          <div id={`slide${index}`} className="carousel-item relative w-full">
+            <img src={image.url} className="w-full aspect-video" />
+            <div className="heading"></div>
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href={`#slide${prevIndex}`} className="btn btn-circle">
+                ❮
+              </a>
+              <a href={`#slide${nextIndex}`} className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ImageCarousel;
