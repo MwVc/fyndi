b7083fe542497d828ff1776ea82efcf30ac306d1
Victor Mwadime <victormwadime@gmail.com>
2026-04-30 21:11:12 +0300

    feat(refresh token models): add deleteToken method to remove refresh tokens from database

--- backend/src/modules/auth/refreshToken.models.ts
+++ backend/src/modules/auth/refreshToken.models.ts
@@ -8,7 +8,7 @@ interface tokenData {
   expiresAt: number;
 }
 
-const insert = async (refreshToken: tokenData, userId: string) => {
+const insertToken = async (refreshToken: tokenData, userId: string) => {
   const { rows } = await pool.query(
     "INSERT INTO refresh_tokens (user_id, token, expires_at, created_at) VALUES ($1, $2, $3, $4) ON CONFLICT (user_id) DO UPDATE SET token = EXCLUDED.token, expires_at = EXCLUDED.expires_at, created_at = EXCLUDED.created_at RETURNING *",
     [
@@ -21,7 +21,7 @@ const insert = async (refreshToken: tokenData, userId: string) => {
   return rows[0];
 };
 
-const get = async (refresh_token: string, user_id: string) => {
+const getToken = async (refresh_token: string, user_id: string) => {
   const { rows } = await pool.query(
     "SELECT EXISTS (SELECT 1 FROM refresh_tokens WHERE token = $1 AND user_id = $2)",
     [refresh_token, user_id],
@@ -30,7 +30,17 @@ const get = async (refresh_token: string, user_id: string) => {
   return rows[0].exists;
 };
 
+const deleteToken = async (refresh_token: string) => {
+  const { rowCount } = await pool.query(
+    "DELETE FROM refresh_tokens WHERE token = $1",
+    [refresh_token],
+  );
+
+  return rowCount;
+};
+
 export const refreshTokenModels = {
-  insert,
-  get,
+  insertToken,
+  getToken,
+  deleteToken,
 };
