6c95bc5cff728d8a776737691aab0a31bb485f7c
Victor Mwadime <victormwadime@gmail.com>
2026-05-13 00:09:21 +0300

    feat: load env config module and update production env check to 'prod'

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,14 @@ import app from "./app.js";
 import { config } from "dotenv";
 import { readFileSync } from "fs";
 import https from "https";
-
+import "./infrastructure/config/env.js";
 
 config(); // load enviroment variables
 
 const PORT = process.env.PORT;
 const isProd = process.env.NODE_ENV;
 
-if (isProd === "production") {
+if (isProd === "prod") {
   app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
 } else {
   // create a https server when in dev mode
