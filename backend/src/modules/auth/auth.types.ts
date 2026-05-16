8cb691b79f8592fd31ea87b62eca91e90ccccb4b
Victor Mwadime <victormwadime@gmail.com>
2026-05-16 22:05:52 +0300

    feat: add SafeUser interface

--- backend/src/modules/auth/auth.types.ts
+++ backend/src/modules/auth/auth.types.ts
@@ -9,3 +9,10 @@ export interface LoginUserInput {
   email: string;
   password: string;
 }
+
+export interface SafeUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "user" | "admin";
+}
