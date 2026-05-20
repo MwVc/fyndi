e3a2957f768f38b38ff0b1fb086911e5f6266a20
Victor Mwadime <victormwadime@gmail.com>
2026-05-20 23:09:51 +0300

    feat: add Passport Google Oauth 2.0 strategy configuration

---
+++ backend/src/modules/auth/oauth/google.strategy.ts
@@ -0,0 +1,30 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import ApiError from "../../../infrastructure/errors/api.errors.js";
+import { AuthErrorCodes } from "../../../infrastructure/errors/code.errors.js";
+
+const clientID = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const callbackURL = process.env.GOOGLE_CALLBACK_URL;
+
+if (!clientID || !clientSecret || !callbackURL) {
+  throw new ApiError(
+    500,
+    AuthErrorCodes.TOKEN_MISSING,
+    "Missing Google Oauth environment variables",
+    false
+  );
+}
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID,
+      clientSecret,
+      callbackURL,
+    },
+    async (accessToken: any, refreshToken: any, profile: any, done: any) => {
+      return done(null, profile);
+    }
+  )
+);
