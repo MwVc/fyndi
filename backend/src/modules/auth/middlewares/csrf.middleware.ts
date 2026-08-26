83b43745033fc57189e56449aac503982784b59a
Victor Mwadime <victormwadime@gmail.com>
2026-08-27 00:20:52 +0300

    refactor: update errorResponse calls to use object parameter syntax in CSRF middleware

--- backend/src/modules/auth/middlewares/csrf.middleware.ts
+++ backend/src/modules/auth/middlewares/csrf.middleware.ts
@@ -5,18 +5,19 @@ import { AuthErrorCodes } from "../../../infrastructure/errors/code.errors.js";
 export const verifyCsrfToken = (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   const csrfCookie: string = req.cookies.csrf_token;
   const csrfHeader = req.header("X-CSRF-TOKEN");
 
   if (!csrfCookie || !csrfHeader || csrfCookie !== csrfHeader) {
-    return errorResponse(
+    return errorResponse({
       res,
-      403,
-      AuthErrorCodes.CSRF_TOKEN_INVALID,
-      "Forbidden",
-    );
+      statusCode: 403,
+      errorCode: AuthErrorCodes.CSRF_TOKEN_INVALID,
+      message: "Forbidden",
+      details: null,
+    });
   }
 
   next();
