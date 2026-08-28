597c51ed3e1fbaed22e3a4d8f208ac0ce2b213a4
Victor Mwadime <victormwadime@gmail.com>
2026-08-28 16:25:22 +0300

    feat: add pino-http HTTP request logging middleware with custom serialisers

---
+++ backend/src/infrastructure/logger/httpLogger.ts
@@ -0,0 +1,21 @@
+import { pinoHttp } from "pino-http";
+import { logger } from "./logger.js";
+
+export const httpLogger = pinoHttp({
+  logger,
+  serializers: {
+    req(req) {
+      return {
+        id: req.id,
+        method: req.method,
+        url: req.url,
+      };
+    },
+
+    res(res) {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+});
