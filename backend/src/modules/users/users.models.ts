7a08f103bf1de885e4a96f0bf2ce4c1edafcb563
Victor Mwadime <victormwadime@gmail.com>
2026-09-01 23:54:35 +0300

    feat: add avatar_url field and insertOauthAccount method with transactional support

--- backend/src/modules/users/users.models.ts
+++ backend/src/modules/users/users.models.ts
@@ -1,4 +1,5 @@
 // import type { RegisterUserData } from "../services/users.services.js";
+import type { Pool, PoolClient } from "pg";
 import pool from "../../infrastructure/db/pgPool.db.js";
 import type {
   DatabaseUser,
@@ -6,16 +7,34 @@ import type {
   PublicUser,
 } from "./users.types.js";
 
-const insert = async ({
-  firstName,
-  lastName,
-  email,
-  password,
-}: InsertUserData) => {
-  const { rows } = await pool.query(
-    "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
-    [firstName, lastName, email, password]
+const insert = async (
+  { firstName, lastName, email, password, avatar }: InsertUserData,
+  client: Pool | PoolClient = pool
+): Promise<DatabaseUser> => {
+  const { rows } = await client.query(
+    "INSERT INTO users (first_name, last_name, email, password, avatar_url) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [firstName, lastName, email, password, avatar]
   );
+  return rows[0];
+};
+
+const insertOauthAccount = async (
+  {
+    user_id,
+    provider,
+    provider_user_id,
+  }: {
+    user_id: string;
+    provider: string;
+    provider_user_id: string;
+  },
+  client: PoolClient
+) => {
+  const { rows } = await client.query(
+    "INSERT INTO oauth_accounts (user_id, provider, provider_user_id) VALUES ($1, $2, $3) RETURNING *",
+    [user_id, provider, provider_user_id]
+  );
+
   return rows[0];
 };
 
@@ -36,4 +55,5 @@ export const userModels = {
   insert,
   getByEmail,
   getById,
+  insertOauthAccount,
 };
