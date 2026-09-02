f2229b0665300a0c576d13ccc69155b2a3fec4c1
Victor Mwadime <victormwadime@gmail.com>
2026-09-02 11:17:02 +0300

    feat: add/me route to users router with access token authentication

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { accessTokenMiddleware } from "../auth/middlewares/verify_access_token.middleware.js";
+import { me } from "./users.controller.js";
 
 const usersRouter = express.Router();
+
+usersRouter.get("/me", accessTokenMiddleware, me);
 
 export default usersRouter;
