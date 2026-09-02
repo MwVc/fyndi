537ec0a329ff9e7e0288b35d24e145de40ce9a57
Victor Mwadime <victormwadime@gmail.com>
2026-09-02 11:10:25 +0300

    refactor: remove imported me controller from auth routes to user routes and remoce unused access token middleware

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -4,13 +4,11 @@ import {
   loginUser,
   refreshUser,
   oauthSignIn,
-  me,
 } from "./auth.controller.js";
 import { logoutUser } from "./auth.controller.js";
 import { refreshTokenMiddleware } from "./middlewares/verify_refresh_token.middleware.js";
 import googleAuthenticate from "./middlewares/google/googleAuthenticate.js";
 import googleCallback from "./middlewares/google/googleCallback.js";
-import { accessTokenMiddleware } from "./middlewares/verify_access_token.middleware.js";
 
 const authRouter = express.Router();
 
@@ -31,7 +29,7 @@ authRouter.get(
 authRouter.post("/register", registerUser);
 authRouter.post("/login", loginUser);
 authRouter.post("/refresh", refreshTokenMiddleware, refreshUser);
-authRouter.get("/me", accessTokenMiddleware, me);
+
 authRouter.post("/logout", refreshTokenMiddleware, logoutUser);
 
 export default authRouter;
